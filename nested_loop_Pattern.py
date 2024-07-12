878752b1595c35e0db9745cdcd8b10c3f86dae4e
Joy <mahedi.hasanjoy1234@gmail.com>
2024-07-12 23:03:46 +0600

    Pattern

---
+++ nested_loop_Pattern.py
@@ -0,0 +1,14 @@
+#Right-Angle Tringle
+
+
+n = 7
+
+    
+for i in range(n):
+    for j in range(1,n-i):
+        print(" ",end="") #space generated
+    for k in range(1,i+2):
+        print(k,end="") # number generated
+    print()
+
+        
